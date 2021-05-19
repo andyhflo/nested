b7058abf21ca7169bd17c3c006e7e8ef30807486
Andrew Florian <floriana18@students.ecu.edu>
2021-05-19 13:30:28 -0400

    setup

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/nested'),
+      dir: require('path').join(__dirname, './coverage/andyhflo'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'nested'`, () => {
+  it(`should have as title 'andyhflo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('nested');
+    expect(app.title).toEqual('andyhflo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('nested app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('andyhflo app is running!');
   });
 });
